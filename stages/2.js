4b8cedee2a3db6aeedd9af3694fa267cefa70912
Gustavo Tardivo <65181580+gutardivo@users.noreply.github.com>
2021-03-11 11:03:37 -0300

    Update 2.js

--- stages/2.js
+++ stages/2.js
@@ -17,6 +17,11 @@ function execute(user, msg) {
     banco.db[user].stage = "ynpattern";
     return ["Aqui está:\n\nApp iOS: https://apps.apple.com/us/app/tinglee/id1539299377\nApp Android: https://play.google.com/store/apps/details?id=com.apptinglee\n\nCertifique-se que seu celular é compatível na lista: shop.tinglee.co/pages/celulares-compativeis\nCaso não seja compatível faça esse processo com outro celular que esteja na lista.\n\nNão se preocupe pois depois da ativação o Tinglee não depende mais do celular\n\n*1* - Isso resolveu\n*2* - Isso não resolveu\n*#* - Para finalizar seu atendimento"];
   }
+  
+  if (msg === "4") {
+    banco.db[user].stage = 0;
+    return ["Certo, neste momento você encosta seu Tinglee na parte *traseira central* do seu celular! Verifique se seu NFC está ativado nas configurações do celular (Na aba Tutorial dentro do app mostra onde está essa opção).\nCaso não aconteça nada mesmo assim, envie um email com o assunto *Preparado para ativar* e seu problema\ncontato@tinglee.co"];
+  }
 
   if (msg === "#") {
     banco.db[user].stage = 0;
@@ -28,7 +33,7 @@ function execute(user, msg) {
     return ["Robô desativado! Caso queira finalizar o atendimento ou voltar ao início digite *#*"]
   }
 
-  if (msg !== '1' && msg !== '2' && msg !== '3' && msg !== '#' && msg !== '!') {
+  if (msg !== '1' && msg !== '2' && msg !== '3' && msg !== '4' && msg !== '#' && msg !== '!') {
     return ["Desculpe não entendi, responda apenas com um caractere de número. *Ex: 1*\nCaso eu não possa te ajudar envie um email para contato@tinglee.co para que um *humano* possa te atender ;)\n\n*#* - Caso queira finalizar o atendimento ou *voltar ao início*"];
   }
 }
